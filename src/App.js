abc2d1e9fa5cb56cc0fe528a6c35fdf4241102f3
nater <n.rome26@gmail.com>
2023-06-07 15:57:38 -0700

    style changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
+import React from 'react';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-3xl underline">
+          This is the new project to work on Rhinos Boxing page. Looks cool
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
