0fe2dd63673eb1a4f17cdbbfe60d1783389f4926
nater <n.rome26@gmail.com>
2023-09-19 13:33:44 -0700

    added assets, components folder, and files

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    <ul>
+        <li>Classes</li>
+        <li>Coaches and Trainers</li>
+        <li>Schedule</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Photo Gallery</li>
+    </ul>
+} 
+
+export default Navbar;
